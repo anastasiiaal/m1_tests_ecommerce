f933b5c91eadb54c2f0ee1ac53cb934b213a60f3
Anastasiia A. <95031679+anastasiiaal@users.noreply.github.com>
2025-03-06 14:26:48 +0100

    add tests

--- tests/Functional/UserControllerTest.php
+++ tests/Functional/UserControllerTest.php
@@ -14,4 +14,29 @@ class UserControllertest extends WebTestCase
         $this->assertResponseIsSuccessful();
         $this->assertJson($client->getResponse()->getContent());
     }
+
+    // on recupère bien un tableau
+    public function testUsersList(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/api/users');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertJson($client->getResponse()->getContent());
+
+        $responseData = json_decode($client->getResponse()->getContent(), true);
+        $this->assertIsArray($responseData, 'La réponse devrait être un tableau');
+    }   
+
+    // ce qu'on recupère est une valeur vide
+    public function testUsersListNotEmpty(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/api/users');
+
+        $this->assertResponseIsSuccessful();
+
+        $responseData = json_decode($client->getResponse()->getContent(), true);
+        $this->assertEmpty($responseData, 'La liste des utilisateurs est vide');
+    }
 }
